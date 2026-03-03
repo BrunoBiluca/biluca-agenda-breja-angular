2cddd0bd4cbb1f5396d25e242a9593896a9d8726
BrunoBiluca <b.b.da.costa@gmail.com>
2026-03-03 18:53:09 -0300

    ✨ Configuração inicial de rotas da aplicação para Login

---
+++ src/auth/login/login.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './login.html',
+})
+export class Login {}
