7850370c7071d58fa7b55611ec9423ddb32f03dc
Bruno Costa <b.b.da.costa@gmail.com>
2026-03-24 18:05:53 -0300

    💄 Alteração do cartão para o componente criado com Spartan

--- src/app/main-page/breweries/breweries.component.ts
+++ src/app/main-page/breweries/breweries.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { BreweriesData } from '@core/breweries/breweries-data.interface';
 import { Brewery } from '@core/breweries/brewery.model';
+import { HlmCardImports } from '@ui/card/src';
 
 @Component({
   selector: 'app-breweries',
   templateUrl: './breweries.component.html',
+  imports: [HlmCardImports],
 })
 export class BreweriesComponent implements OnInit {
   breweries = signal<Brewery[] | undefined>(undefined);
